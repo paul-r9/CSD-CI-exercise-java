ab1fdd37578f8a3620442008f95d742473689606
Paul Moore <pdmoore@gmail.com>
2020-10-13 13:33:25 -0700

    revert to Junit 4.13.1, confirm azure detects & runs tests

--- src/test/java/com/rocketnine/ISBN10Test.java
+++ src/test/java/com/rocketnine/ISBN10Test.java
@@ -1,11 +1,10 @@
 package com.rocketnine;
 
 import dev.emergent.BookInfo;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 
 public class ISBN10Test {
