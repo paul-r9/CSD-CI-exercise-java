5e907f7b84417d57e0257876226742edc0d4dc84
myang97 <myang97@utexas.edu>
2021-03-04 15:39:53 -0600

    rename class

--- src/test/java/com/rocketnine/ISBN13ChecksumTest.java
+++ src/test/java/com/rocketnine/ISBN13ChecksumTest.java
@@ -8,10 +8,23 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 
-public class ISBN13Checksum {
+public class ISBN13ChecksumTest {
+//    @Test
+//    public void FailingTest_BreaksTheBuild() {
+//        fail("Azure build regarding ISBN13Checksum is broken. Delete this test to get a Green build.");
+//    }
+
     @Test
-    public void FailingTest_BreaksTheBuild() {
-        fail("Azure build regarding ISBN13Checksum is broken. Delete this test to get a Green build.");
+    public void ISBN_shorterThan10Characters_ReturnsInvalidBookInfo() {
+        // Arrange
+        ISBNFinder sut = new ISBNFinder();
+
+        // Act
+        String shortISBN = "12345";
+        BookInfo actual = sut.lookup(shortISBN);
+
+        // Assert
+        assertEquals("ISBN must be 10 characters in length", actual.title);
     }
 
 //    @Test
