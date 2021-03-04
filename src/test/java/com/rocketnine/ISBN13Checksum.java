29f54f2618cd1a8a31c87c02d122ff5d18e3b64e
myang97 <myang97@utexas.edu>
2021-03-04 15:22:17 -0600

    added test to fail for isbn13checksum

---
+++ src/test/java/com/rocketnine/ISBN13Checksum.java
@@ -0,0 +1,29 @@
+package com.rocketnine;
+
+import dev.emergent.BookInfo;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+
+public class ISBN13Checksum {
+    @Test
+    public void FailingTest_BreaksTheBuild() {
+        fail("Azure build regarding ISBN13Checksum is broken. Delete this test to get a Green build.");
+    }
+
+//    @Test
+//    public void ISBN_shorterThan10Characters_ReturnsInvalidBookInfo() {
+//        // Arrange
+//        ISBNFinder sut = new ISBNFinder();
+//
+//        // Act
+//        String shortISBN = "12345";
+//        BookInfo actual = sut.lookup(shortISBN);
+//
+//        // Assert
+//        assertEquals("ISBN must be 10 characters in length", actual.title);
+//    }
+}
