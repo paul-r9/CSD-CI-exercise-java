ec7066827b5260884e62810b40ae73956129ea7b
giribabuthammisetty <giribabu.thammisetty@exactuals.com>
2021-12-16 16:49:09 -0500

    added additional test.

--- src/test/java/com/rocketnine/ISBN13ChecksumTest.java
+++ src/test/java/com/rocketnine/ISBN13ChecksumTest.java
@@ -1,7 +1,11 @@
 package com.rocketnine;
 
+import dev.emergent.BookInfo;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class ISBN13ChecksumTest {
 
@@ -10,4 +14,18 @@ public class ISBN13ChecksumTest {
         ISBNFinder sut = new ISBNFinder();
         Assertions.assertTrue(sut.checkSum13());
     }
+
+    @Test
+    public void ISBN13_shorterThan10Characters_ReturnsInvalidBookInfo() {
+        // Arrange
+        ISBNFinder sut = new ISBNFinder();
+
+        // Act
+        String shortISBN = "1234544444444444";
+        BookInfo actual = sut.lookup(shortISBN);
+
+        // Assert
+        assertEquals("ISBN must be 10 characters in length", actual.title);
+    }
+
 }
