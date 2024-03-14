000edd45faed3e46df8d4f3f9fc7ea177d6264e3
Collange, Etienne <etienne.collange@cengage.com>
2024-03-14 16:04:51 -0400

    added test of retrieval of actual book with isbn 13

--- src/test/java/com/rocketnine/ISBN13FormatTest.java
+++ src/test/java/com/rocketnine/ISBN13FormatTest.java
@@ -2,6 +2,8 @@ package com.rocketnine;
 
 import dev.emergent.BookInfo;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -17,11 +19,12 @@ public class ISBN13FormatTest {
 
     }
 
-    @Test
-    void ISBNFinderRejectsInputLongerThan13Digits() {
+    @ParameterizedTest(name = "{0}")
+    @CsvSource({"9380135957053454359", "45645645", "35235678887"})
+    void ISBNFinderRejectsInputDifferentThan10or13Digits(String isbn) {
         ISBNFinder sut = new ISBNFinder();
 
-        BookInfo bookInfo = sut.lookup("9380135957053454359");
+        BookInfo bookInfo = sut.lookup(isbn);
         assertEquals(new BookInfo("ISBN must be 10 or 13 characters in length").toString(), bookInfo.toString());
     }
 
@@ -41,5 +44,18 @@ public class ISBN13FormatTest {
         assertEquals(new BookInfo("Title not found").toString(), bookInfo.toString());
     }
 
+    @Test
+    void ISBNFinderRetrieveBookFromExistingIsbn13() {
+        ISBNFinder sut = new ISBNFinder();
+
+        BookInfo actual = sut.lookup("978 0596-80948 5");
+        BookInfo expected = new BookInfo(
+                "97 Things Every Programmer Should Know",
+                "Kevlin Henney",
+                "0596809484",
+                "9780596809485"
+        );
+        assertEquals(expected.toString(), actual.toString());
+    }
 
 }
