65277212d2797323de013057357134384f1a1abe
sldoull <slkarpie@gmail.com>
2024-03-14 15:45:36 -0400

    First valid checksum test

---
+++ src/test/java/com/rocketnine/ISBN13ChecksumTest.java
@@ -0,0 +1,27 @@
+package com.rocketnine;
+
+import dev.emergent.BookInfo;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+
+class ISBN13ChecksumTest {
+    
+    @Test
+    void Valid_Checksum() {
+        // Arrange
+        ISBNFinder sut = new ISBNFinder();
+
+        // Act
+        String ISBN = "0321146530";
+        BookInfo actual = sut.lookup(ISBN);
+
+        // Assert
+        //assertEquals(0, actual.toString().substring(actual.toString().length()-1, actual.toString().length()));
+        assertEquals('0', actual.isbn10().charAt(9));
+    }
+
+}
