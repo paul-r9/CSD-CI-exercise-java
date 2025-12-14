5c27f8480425d405bba7af3e75099d254daf3415
Paul Moore <pdmoore@gmail.com>
2025-12-14 11:25:45 -0800

    ignore the failing test, expect ADO build to fail

--- src/test/java/com/rocketnine/ISBN10Test.java
+++ src/test/java/com/rocketnine/ISBN10Test.java
@@ -1,6 +1,7 @@
 package com.rocketnine;
 
 import dev.emergent.BookInfo;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -58,7 +59,7 @@ public class ISBN10Test {
     }
 
     @Test
-//    @Disabled("Expect CI build to pass when this test is not run")
+    @Disabled("Expect CI build to pass when this test is not run")
     public void FailingTest_BreaksTheBuild() {
         fail("Azure build should fail when running this test. Delete this test to get a Green build.");
     }
